3de402e8980a44da48d108746ba59e3575f23898
codeguru <codeguru@users.sourceforge.net>
2014-06-23 23:46:23 -0600

    Fix problems with package name clashes

--- app/src/main/java/codeguru/checkablelinearlayout/example/MainActivity.java
+++ app/src/main/java/codeguru/checkablelinearlayout/example/MainActivity.java
@@ -1,10 +1,9 @@
-package codeguru.checkablelinearlayout;
+package codeguru.checkablelinearlayout.example;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import codeguru.checkablelinearlayout.R;
 
 public class MainActivity extends ActionBarActivity {
 
